22bfebe3f2a3d45abfc238a075a1ada54eed4778
NamuraCode <dnaranjo0@misena.edu.co>
2021-10-28 00:39:36 -0500

    productos3

--- src/app.js
+++ src/app.js
@@ -15,3 +15,24 @@ app.get('/',(req,res)=>{
 app.get('/productCart',(req,res)=>{
     res.sendFile(path.join(__dirname,'./view/productCart.html'))
 });
+
+app.get('/products',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./view/products.html'))
+});
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./view/login.html'))
+});
+
+app.get('/productDetail',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./view/productDetail.html'))
+});
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./view/register.html'))
+});
+
+app.get('/index',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./view/index.html'))
+});
+

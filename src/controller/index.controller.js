a921d947a2293770fe32393f0a4e72ec36e42ad5
NamuraCode <dnaranjo0@misena.edu.co>
2021-11-29 17:32:47 -0500

    Se agrego controller post create

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -6,43 +6,57 @@ controller = {
         res.render('index')
     },
 
-    productCart:(req,res)=>{
+    productCart: (req, res) => {
         res.render('productCart')
     },
 
-    products:(req,res)=>{
-        res.render('products',{"ingresan":users})
+    products: (req, res) => {
+        res.render('products', {
+            "ingresan": users
+        })
     },
 
-    login:(req,res)=>{
+    login: (req, res) => {
         res.render('login')
     },
 
-    productDetail:(req,res)=>{
+    productDetail: (req, res) => {
         let id = req.params.id
         console.log(id)
-        res.render('productDetail',{"ingresan":users, "id":id})
+        res.render('productDetail', {
+            "ingresan": users,
+            "id": id
+        })
     },
 
-    register:(req,res)=>{
+    register: (req, res) => {
         res.render('register')
     },
 
-    productForm:(req,res)=>{
+    productForm: (req, res) => {
         res.render('productForm')
     },
 
-    addProduct:(req,res) =>{
+    addProduct: (req, res) => {
         res.render('addProduct')
     },
 
-    editProduct:(req,res) =>{
+    editProduct: (req, res) => {
         res.render('editProduct')
     },
 
-    removeProduct:(req,res) =>{
+    removeProduct: (req, res) => {
         res.render('removeProduct')
     },
+
+    create: (req, res) => {
+        let creating={
+            name:req.body.product,
+            price:req.body.price
+        }
+        console.log(creating)
+        res.redirect('/productForm')
+    }
 }
 
 module.exports = controller;

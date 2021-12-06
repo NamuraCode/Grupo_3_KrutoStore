e5a79c0a3866ece161af1eaa28c53c77416f6211
unknown <cristinagil82@gmail.com>
2021-12-06 09:46:20 -0500

    Add: Ruta de usuarios

--- src/routers/users.routes.js
+++ src/routers/users.routes.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const usersController = require('../controller/userController');
+const indexController = require('../controller/indexController');
+const multer = require('multer');
+const {body} = require('express-validator');

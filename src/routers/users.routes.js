e8c06bb028fd28526a0555cc120a1eb168a1840e
unknown <cristinagil82@gmail.com>
2021-12-06 09:47:23 -0500

    Add: Ruta de registro

--- src/routers/users.routes.js
+++ src/routers/users.routes.js
@@ -5,3 +5,20 @@ const usersController = require('../controller/userController');
 const indexController = require('../controller/indexController');
 const multer = require('multer');
 const {body} = require('express-validator');
+
+
+
+
+// Formulario de registro
+router.get('/register', usersController.register);
+
+// Procesar el registro
+router.post('/register', uploadFile.single('avatar'), validations, usersController.processRegister);
+
+// Formulario de Login
+router.get('/login', usersController.login);
+
+// Perfil de usuario
+router.get('/profile/:userId', usersController.profile);
+
+module.exports = router; 

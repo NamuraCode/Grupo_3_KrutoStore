aa5460b0e5743d048efde328b77fe8c473967c59
unknown <cristinagil82@gmail.com>
2021-12-16 21:46:09 -0500

    correciones al login

--- src/controller/usersControllers.js
+++ src/controller/usersControllers.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
+const bcrypt = require('bcryptjs');
 const products = require('./model/users.json');
 const { validationResult } = require('express-validator');
 
 const userController = {
     register: (req, res) => {
-        return res.render ('index');
+        res.render(path.resolve(__dirname, '../views/usuarios/register'));
     },
     login: (req, res) => {
-        return res.render ('userLoginForm');
+        res.render(path.resolve(__dirname, '../views/usuarios/login')); 
     },
     profile: (req, res) => {
         return res.render ('userProfile');

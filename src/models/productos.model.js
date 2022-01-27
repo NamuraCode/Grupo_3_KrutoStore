3c6d0ba9295440e5975f67e55d49af373d83cb17
Salome Gomez <sgomez713@misena.edu.co>
2022-01-26 20:28:00 -0500

    commit por precaucion

--- src/models/productos.model.js
+++ src/models/productos.model.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const db = require('../database/models')
 
 const metodosProductos = {
@@ -9,6 +11,45 @@ const metodosProductos = {
             return productos
         } catch (error) {
             return console.log(error)
+        }
+    },
+
+    newProductos: async function (Producto, files) {
+        try {
+            const images = [];
+
+            if (files["imagesUpload"]) {
+                images = files["imagesUpload"].map((image) => {
+                    return image.filename;
+                });
+            }
+
+            await db.Productos.create({
+                productName: Producto.nombre,
+                productCategory: Producto.categorias_id,
+                productPrice: Producto.precio,
+                productDescription: Producto.descripcion
+            })
+        } catch (error) {
+            console.error(error);}
+    },
+    editProductos: async function (Producto) {
+        try {
+            await db.Productos.edit({
+                productName: Producto.nombre,
+                productCategory: Producto.categorias_id,
+                productPrice: Producto.precio,
+                productDescription: Producto.descripcion
+            })
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    deleteProductos: async function (id) {
+        try {
+            await db.Productos.destroy({where: {id:id}})
+        } catch (error) {
+            console.error(error);
         }
     }
 }

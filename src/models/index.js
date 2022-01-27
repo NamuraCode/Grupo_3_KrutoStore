a22a1d310199a5d6821dc3438d77d19012a8bc93
Salome Gomez <sgomez713@misena.edu.co>
2022-01-27 16:42:17 -0500

    se logro conectar a la base de datos de productos

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+require('dotenv').config();
 
 const productosLogica = require(path.resolve(__dirname, './productos.model'))
 const usuariosLogica = require(path.resolve(__dirname, './usuarios.model'))
--- src/models/productos.model.js
+++ src/models/productos.model.js
@@ -1,57 +1,69 @@
-const fs = require('fs');
-const path = require('path');
 const db = require('../database/models')
 
-const metodosProductos = {
-    getAll: async function () {
+const productModel = {
+    getAll: async () => {
         try {
-            let productos = await db.Productos.findAll({
-                include: ["categorias", "usuarios", "imagenes"]
-            })
-            return productos
+            const result = await db.Productos.findAll()
+            console.log(result);
         } catch (error) {
-            return console.log(error)
+            console.log('Ocurrio un error ${error.message}');
         }
     },
-
-    newProductos: async function (Producto, files) {
-        try {
-            const images = [];
-
-            if (files["imagesUpload"]) {
-                images = files["imagesUpload"].map((image) => {
-                    return image.filename;
-                });
-            }
-
-            await db.Productos.create({
-                productName: Producto.nombre,
-                productCategory: Producto.categorias_id,
-                productPrice: Producto.precio,
-                productDescription: Producto.descripcion
-            })
-        } catch (error) {
-            console.error(error);}
-    },
-    editProductos: async function (Producto) {
-        try {
-            await db.Productos.edit({
-                productName: Producto.nombre,
-                productCategory: Producto.categorias_id,
-                productPrice: Producto.precio,
-                productDescription: Producto.descripcion
-            })
-        } catch (error) {
-            console.error(error);
-        }
-    },
-    deleteProductos: async function (id) {
-        try {
-            await db.Productos.destroy({where: {id:id}})
-        } catch (error) {
-            console.error(error);
-        }
-    }
 }
 
-module.exports = metodosProductos
+
+
+
+// const metodosProductos = {
+//     getAll: async function () {
+//         try {
+//             let productos = await db.Productos.findAll({
+//                 include: ["categorias", "usuarios", "imagenes"]
+//             })
+//             return productos
+//         } catch (error) {
+//             return console.log(error)
+//         }
+//     },
+
+//     newProductos: async function (Producto, files) {
+//         try {
+//             const images = [];
+
+//             if (files["imagesUpload"]) {
+//                 images = files["imagesUpload"].map((image) => {
+//                     return image.filename;
+//                 });
+//             }
+
+//             await db.Productos.create({
+//                 productName: Producto.nombre,
+//                 productCategory: Producto.categorias_id,
+//                 productPrice: Producto.precio,
+//                 productDescription: Producto.descripcion
+//             })
+//         } catch (error) {
+//             console.error(error);}
+//     },
+//     editProductos: async function (Producto) {
+//         try {
+//             await db.Productos.edit({
+//                 productName: Producto.nombre,
+//                 productCategory: Producto.categorias_id,
+//                 productPrice: Producto.precio,
+//                 productDescription: Producto.descripcion
+//             })
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     },
+//     deleteProductos: async function (id) {
+//         try {
+//             await db.Productos.destroy({where: {id:id}})
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     }
+// }
+
+// module.exports = metodosProductos

f82e52e3a5a1cb312525f3f68703697a9dad8f7f
David Naranjo <dnaranjo0@misena.edu.co>
2022-01-26 19:41:40 -0500

    Boton buy now del detalle del producto arreglado para redireccionar

--- src/middlewares/adminSessionMiddleware.js
+++ src/middlewares/adminSessionMiddleware.js
@@ -3,7 +3,7 @@ function authMiddleware(req, res, next) {
         if (req.session.user != undefined && req.session.user.perfiles_id == 1) {
             next()
         } else {
-            res.send('Esta pagina es solo para Administradores')
+            res.redirect('./register')
         }
     } catch (error) {
         next(error)

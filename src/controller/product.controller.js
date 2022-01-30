b306843010f1d7564642cf896151e32c12814b63
Salome Gomez <sgomez713@misena.edu.co>
2022-01-29 19:21:40 -0500

    commit para cambiar de branch

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,4 +1,4 @@
-const models = require('../models');
+const models = require('../models/index');
 
 const productController = {
     agregarProducto: async(req, res, next) => {
--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,10 @@
 const path = require('path');
 require('dotenv').config();
 
-const productosLogica = require(path.resolve(__dirname, './productos.model'))
+const productModel = require(path.resolve(__dirname, './productos.model'))
 const usuariosLogica = require(path.resolve(__dirname, './usuarios.model'))
 
 module.exports = {
-        productosLogica,
+        productModel,
         usuariosLogica
 }

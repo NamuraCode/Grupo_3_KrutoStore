261e30090a6786b685553afa9e6b6e4d9509fc56
Salome Gomez <sgomez713@misena.edu.co>
2022-03-01 17:44:54 -0500

    se optimizo el CSS de Index

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,16 @@
 require("dotenv").config()
 module.exports = {
     development: {
-        "username": process.env.DBUSER,
-        "password": process.env.DBPASS,
-        "database": process.env.DBNAME,
-        "host": process.env.DBHOST,
-        "dialect": "mysql",
+        "username": "franAdmin",
+        "password": "Adrian12234..",
+        "database": "Kruto_Store_Grupo3",
+        "host": "45.79.201.214",
+        "dialect": "mysql"
+        // "username": process.env.DBUSER,
+        // "password": process.env.DBPASS,
+        // "database": process.env.DBNAME,
+        // "host": process.env.DBHOST,
+        // "dialect": "mysql",
     },
     test: {
         "username": process.env.DBUSER,
